0af5e1fdf6f6f84b6a741827d196f6350c7e92b4
Gans <angel9899@mail.ru>
2023-07-18 13:58:14 +0300

    End of layout under 1300

---
+++ src/components/footer/footer.js
@@ -0,0 +1,41 @@
+
+
+
+export function Footer(){
+    return(
+        <footer className='footer'>
+            <div className='footer-container container'>
+                <div className='footer-about'>
+                    <div className='footer-img'>
+                        <a href=""><img src="img/logo-footer.png" alt="logo"/></a>
+                    </div>
+                    <div>
+                        <address className='footer-adress'>
+                            <ul>
+                                <li><a href="">+7 (495) 984 25 13</a></li>
+                                <li><a href="">info@neoflex.ru</a></li>
+                            </ul>
+                        </address>
+                    </div>
+                </div>
+                <div>
+                    <nav className='footer-links'>
+                        <a href="">About bank</a>
+                        <a href="">Ask a Question</a>
+                        <a href="">Quality of service</a>
+                        <a href="">Requisites</a>
+                        <a href="">Press center</a>
+                        <a href="">Bank career</a>
+                        <a href="">Investors</a>
+                        <a href="">Analytics</a>
+                        <a href="">Business and processes</a>
+                        <a href="">Compliance and business ethics</a>
+                    </nav>
+                </div>
+                <div className='footer-text'>
+                    <p>We use cookies to personalize our services and improve the user experience of our website. Cookies are small files containing information about previous visits to a website. If you do not want to use cookies, please change your browser settings</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import {WhatFeaturesDoWeProvide} from "../components/what_features_do_we_provide
 import {ExchangeRate} from "../components/exchange_rate/exchange_rate";
 import {ServicesWorld} from "../components/services_anywhere_in_the_world/services_anywhere_in_the_world";
 import {Subscribe} from "../components/subscribe/subscribe";
+import {Footer} from "../components/footer/footer";
 
 export function Home (props){
     return(
@@ -14,6 +15,7 @@ export function Home (props){
             <ExchangeRate />
             <ServicesWorld />
             <Subscribe />
+            <Footer />
         </div>
     )
 }
