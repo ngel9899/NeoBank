5a948dd35758071791615e3c7d07ea23f3b887cd
Gans <angel9899@mail.ru>
2023-10-04 14:17:11 +0300

    Final

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,4 @@
 import { Header } from '../components/header/header';
-import { ContinuationOfTheApplication } from '../components/continuationOfTheApplication/continuationOfTheApplication';
 import { Footer } from '../components/footer/footer';
 import React from 'react';
 import { Error404 } from '../components/error404/error404';
